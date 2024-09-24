e1fb80386f30e3beb46fe86b14cc9385a4450ed6
Paweł <p.grabczak@cognitionhub.com>
2024-09-24 18:20:40 +0200

    literówka

--- to-do-list/src/components/Container/Container.js
+++ to-do-list/src/components/Container/Container.js
@@ -2,7 +2,7 @@ import style from './Container.module.scss';
 
 const Container = props => {
     return (
-        <div className={style.Container}>{props.children}</div>//skup się na tym co robisz w środku zawsze jest props children
+        <div className={style.container}>{props.children}</div>//skup się na tym co robisz w środku zawsze jest props children
     );
 };
 
